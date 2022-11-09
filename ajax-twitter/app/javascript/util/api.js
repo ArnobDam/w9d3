832d6f39c095efa2b7005e74160e9cceef19ccd5
Arnob <arnobdam@gmail.com>
2022-11-09 17:02:31 -0500

    api.js

--- ajax-twitter/app/javascript/util/api.js
+++ ajax-twitter/app/javascript/util/api.js
@@ -4,10 +4,22 @@ async function customFetch(url, options = {}) {
   options.headers = {
     // Your code here
     "X-CSRF-Token": csrfToken,
+
+    Accept: "application/json",
+
     ...options.headers,
   };
 
-  return await fetch(url, options);
+  // return await fetch(url, options);
+  const response = fetch(url, options).then(response => response.json(), (response) => { throw response });
+  return response;
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw response;
+  }
+
+
 }
 
 export function followUser(id) {
